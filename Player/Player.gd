ae4aa8801b93dd71fc1a1d6ea1b76265e1828ef3
Trevor <trevor.meest@gmail.com>
2022-10-03 01:18:08 -0600

    End of Video 7. Added Dirt Path TileSet

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,34 +10,34 @@ var velocity = Vector2.ZERO
 # Gets the variable from a child node in the scene when the Player node
 # is ready. This makes sure that the AnimationPlayer will be loaded before
 # setting the variable.
-onready var animationPlayer = $AnimationPlayer
-onready var animationTree = $AnimationTree
+onready var animation_player = $AnimationPlayer
+onready var animation_tree = $AnimationTree
 
 # This variable gets the actual Tree itself so that we can move between our
 # different blendspace2Ds using .travel()
-onready var animationState = animationTree.get("parameters/playback")
+onready var animation_state = animation_tree.get("parameters/playback")
 
 func _physics_process(_delta):
 	
-	var inputVector = Vector2.ZERO
+	var input_vector = Vector2.ZERO
 	
-	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	inputVector = inputVector.normalized()
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 		
 	# This if/else handles setting our player move speed. 
 	# If a direction is being pressed we will move our player toward it.
 	# Else we will start to reduce our player speed towards zero.
-	if (inputVector != Vector2.ZERO):
+	if (input_vector != Vector2.ZERO):
 		
 		# Set the blend position once we have an input vector from the player
 		# so that we don't set the position to 0 as that can cause issues.
-		animationTree.set("parameters/Idle/blend_position", inputVector)
-		animationTree.set("parameters/Run/blend_position", inputVector)
-		animationState.travel("Run")
-		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION)
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_state.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION)
 	else:
-		animationState.travel("Idle")
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 		
 	# Use the velocity = ... part in order to make it
