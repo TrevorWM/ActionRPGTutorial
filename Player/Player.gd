f372a624898befb016277153956c4f3bea644ff2
Trevor <trevor.meest@gmail.com>
2022-10-03 00:29:57 -0600

    End of video 6. AnimationTree set up for Player

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,15 @@ const MAX_SPEED = 100
 
 var velocity = Vector2.ZERO
 
+# Gets the variable from a child node in the scene when the Player node
+# is ready. This makes sure that the AnimationPlayer will be loaded before
+# setting the variable.
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+
+# This variable gets the actual Tree itself so that we can move between our
+# different blendspace2Ds using .travel()
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(_delta):
 	
@@ -20,8 +29,15 @@ func _physics_process(_delta):
 	# If a direction is being pressed we will move our player toward it.
 	# Else we will start to reduce our player speed towards zero.
 	if (inputVector != Vector2.ZERO):
+		
+		# Set the blend position once we have an input vector from the player
+		# so that we don't set the position to 0 as that can cause issues.
+		animationTree.set("parameters/Idle/blend_position", inputVector)
+		animationTree.set("parameters/Run/blend_position", inputVector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 		
 	# Use the velocity = ... part in order to make it
