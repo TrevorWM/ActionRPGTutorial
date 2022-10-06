bcdbd5bf5b8e12ec9a798729fbddb0856f32976b
Trevor <trevor.m@hotmail.ca>
2022-10-05 22:34:08 -0600

    End video 9, added attack animations and state enum

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,7 +4,13 @@ const ACCELERATION = 10
 const FRICTION = 10
 const MAX_SPEED = 100
 
+enum State {
+	MOVE,
+	ROLL,
+	ATTACK,
+}
 
+var current_state = State.MOVE
 var velocity = Vector2.ZERO
 
 # Gets the variable from a child node in the scene when the Player node
@@ -17,8 +23,22 @@ onready var animation_tree = $AnimationTree
 # different blendspace2Ds using .travel()
 onready var animation_state = animation_tree.get("parameters/playback")
 
-func _physics_process(_delta):
+func _ready():
+	animation_tree.active = true
+
+func _physics_process(delta):
 	
+	match current_state:
+		State.MOVE:
+			move_state(delta)
+		State.ROLL:
+			pass
+		State.ATTACK:
+			attack_state()
+	
+	
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -34,6 +54,7 @@ func _physics_process(_delta):
 		# so that we don't set the position to 0 as that can cause issues.
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_tree.set("parameters/Attack/blend_position", input_vector)
 		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION)
 	else:
@@ -44,3 +65,14 @@ func _physics_process(_delta):
 	# so that the player character doesn't wiggle when
 	# walking into a corner.
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		current_state = State.ATTACK
+		velocity = Vector2.ZERO
+	
+	
+func attack_state():
+	animation_state.travel("Attack")
+	
+func attack_animation_finished():
+	current_state = State.MOVE
