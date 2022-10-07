413ac28ece39e1cf5d61a61a0f9b010a4a4169c4
Trevor <trevor.m@hotmail.ca>
2022-10-07 15:29:57 -0600

    End video 12. Added roll animation and code to player.

--- Player/Player.gd
+++ Player/Player.gd
@@ -12,6 +12,7 @@ enum State {
 
 var current_state = State.MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.DOWN
 
 # Gets the variable from a child node in the scene when the Player node
 # is ready. This makes sure that the AnimationPlayer will be loaded before
@@ -32,7 +33,7 @@ func _physics_process(delta):
 		State.MOVE:
 			move_state(delta)
 		State.ROLL:
-			pass
+			roll_state()
 		State.ATTACK:
 			attack_state()
 
@@ -43,17 +44,21 @@ func move_state(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-		
+	
+	
 	# This if/else handles setting our player move speed. 
 	# If a direction is being pressed we will move our player toward it.
 	# Else we will start to reduce our player speed towards zero.
 	if (input_vector != Vector2.ZERO):
+		
+		roll_vector = input_vector
 		
 		# Set the blend position once we have an input vector from the player
 		# so that we don't set the position to 0 as that can cause issues.
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
 		animation_tree.set("parameters/Attack/blend_position", input_vector)
+		animation_tree.set("parameters/Roll/blend_position", input_vector)
 		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -63,15 +68,30 @@ func move_state(delta):
 	# Use the velocity = ... part in order to make it
 	# so that the player character doesn't wiggle when
 	# walking into a corner.
-	velocity = move_and_slide(velocity)
+	move()
+	
+	if Input.is_action_just_pressed("Roll"):
+		current_state = State.ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		current_state = State.ATTACK
 		velocity = Vector2.ZERO
 	
-	
+func move():
+	velocity = move_and_slide(velocity)
+
 func attack_state():
 	animation_state.travel("Attack")
 	
 func attack_animation_finished():
 	current_state = State.MOVE
+	
+func roll_state():
+	velocity = roll_vector * MAX_SPEED * 1.5
+	move()
+	animation_state.travel("Roll")
+	
+func roll_animation_finished():
+	current_state = State.MOVE
+	# Prevents microstutter of player movement at the end of a roll
+	velocity = velocity * 0.70
