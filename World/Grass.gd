503d5fb8ccaf36f61ea6c131f07db6d942a146f9
Trevor <trevor.m@hotmail.ca>
2022-10-08 21:50:30 -0600

    End video 15. Code refactoring and adding bat death effect.

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,18 +1,16 @@
 extends Node2D
 
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
+
 func create_grass_effect():
 	
 #	This loads the grass destroy animation in the same position as
 #	the grass object that was destroyed. It loads the animation scene
 #	then creates an instance of the animation at the global position
 #	of the current grass object.
-	var GrassEffect = load("res://Effects/GrassEffect.tscn")
 	var grass_effect = GrassEffect.instance()
-	var world = get_tree().current_scene
-	
-	world.add_child(grass_effect)
+	get_parent().add_child(grass_effect)
 	grass_effect.global_position = global_position
-	
 	queue_free()
 
 
